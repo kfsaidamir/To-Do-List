171cbb5f88fb4f2c69f66a2663c78944d01e14ed
kf_saidamir <skadirov2009@gmail.com>
2024-05-05 10:17:33 +0500

    A

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const registrationForm = document.querySelector('.registrationForm');
   const loginButton = document.querySelector('.loginButton'); 
 
-  const rainSound = document.getElementById('rainSound');
-  rainSound.play();
 
   document.querySelector('.username').value = '';
   document.querySelector('.password').value = '';
@@ -21,7 +19,6 @@ document.addEventListener('DOMContentLoaded', function () {
       if (users[username]) {
         showMessage('Username already exists. Please choose another one.');
       } else {
-        // Запрашиваем подтверждение регистрации
         const confirmRegistration = confirm('Are you sure you want to register with the provided credentials?');
         if (confirmRegistration) {
           users[username] = password;
@@ -43,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const username = document.querySelector('.username').value;
     const password = document.querySelector('.password').value;
 
-    // Получаем данные пользователей из Local Storage
     let users = localStorage.getItem('users');
     users = users ? JSON.parse(users) : {};
 
@@ -51,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
       showMessage('Login successful!');
       setTimeout(function () {
         window.location.href = 'index2.html';
-      }, 2000);
+      }, 1200);
     } else {
       showMessage('Incorrect username or password. Please try again.');
     }
